bb603952e6a483a7df84752e91876edfe8500b3a
KAPIL KUMAR <dekapx@KAPILs-MacBook-Pro-M2.local>
2025-10-04 14:19:31 +0100

    weather-app initial version

---
+++ weather-app/src/main/java/com/dekapx/weatherapp/controller/SensorController.java
@@ -0,0 +1,22 @@
+package com.dekapx.weatherapp.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.dekapx.weatherapp.common.ResourceUrls.BASE_URL;
+import static com.dekapx.weatherapp.common.ResourceUrls.INFO_URL;
+
+@Slf4j
+@RestController
+@RequestMapping(BASE_URL)
+@RequiredArgsConstructor
+public class SensorController {
+    @GetMapping(INFO_URL)
+    public String getInfo() {
+        log.info("Weather API v1.0");
+        return "Weather API v1.0";
+    }
+}
